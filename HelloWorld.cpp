82dda03291a6681abd4e9ddb53606e2bf55b1da9
Ali Said <ali.bilal.said@hotmail.com>
2022-01-28 16:37:37 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,7 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Ali Said <ali.bilal.said@hotmail.com>
+
+// Header file for input output functions
 #include<iostream>  
   
 using namespace std; 
